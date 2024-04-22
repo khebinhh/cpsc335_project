e952fb2b8c745eaab1f4f440315e4d4354383f31
Kevin <60121349+khebinhh@users.noreply.github.com>
2024-04-22 20:28:29 +0000

    start of bfs.py and dfs.py

---
+++ project/bfs.py
@@ -0,0 +1,10 @@
+# Use BFS to find the shortest path by the number of edges (stops).
+def bfs(graph, start, end):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == end:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
